9eba528a9d85b0829dc07a8e44fd5a3f336e164a
Roel <10514742+rboonzaijer@users.noreply.github.com>
2025-11-21 17:35:10 +0100

    add tests for php 8.5

--- docker-run-tests.sh
+++ docker-run-tests.sh
@@ -8,4 +8,5 @@ docker compose run --rm php8.1 php ./vendor/bin/phpunit -c tests/phpunit-8.1.xml
 docker compose run --rm php8.2 php ./vendor/bin/phpunit -c tests/phpunit-8.2.xml && \
 docker compose run --rm php8.3 php ./vendor/bin/phpunit -c tests/phpunit-8.3.xml && \
 docker compose run --rm php8.4 php ./vendor/bin/phpunit -c tests/phpunit-8.4.xml && \
+docker compose run --rm php8.5 php ./vendor/bin/phpunit -c tests/phpunit-8.5.xml && \
 echo -e "\n\ndone.\n"
